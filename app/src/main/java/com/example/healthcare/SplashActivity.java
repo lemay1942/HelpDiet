2043b7257c51a3668e37b36674e5a1c0c6be0612
Min-gyu Kim <lemay1942@gmail.com>
2019-08-29 12:49:19 +0900

    123
    456

---
+++ app/src/main/java/com/example/healthcare/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goToMainActivity();
+            }
+        },2000);
+
+    }
+
+    private void goToMainActivity() {
+        Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(mainActivityIntent);
+        finish();
+    }
+}
