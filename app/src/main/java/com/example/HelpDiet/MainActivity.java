bc5895505379d48d48bbd5c90acbcc4a35744efa
Min-gyu Kim <lemay1942@gmail.com>
2019-09-10 13:33:09 +0900

    onBackPressed
    333

--- app/src/main/java/com/example/HelpDiet/MainActivity.java
+++ app/src/main/java/com/example/HelpDiet/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.HelpDiet;
 
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -53,5 +55,29 @@ public class MainActivity extends AppCompatActivity {
         machineBtn.setOnClickListener(onClickListner);
         calendarBtn.setOnClickListener(onClickListner);
         chartBtn.setOnClickListener(onClickListner);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // AlertDialog 빌더를 이용해 종료시 발생시킬 창을 띄운다
+        AlertDialog.Builder alBuilder = new AlertDialog.Builder(this);
+        alBuilder.setMessage("종료하시겠습니까?");
+
+        // "예" 버튼을 누르면 실행되는 리스너
+        alBuilder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish(); // 현재 액티비티를 종료한다. (MainActivity에서 작동하기 때문에 애플리케이션을 종료한다.)
+            }
+        });
+        // "아니오" 버튼을 누르면 실행되는 리스너
+        alBuilder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return; // 아무런 작업도 하지 않고 돌아간다
+            }
+        });
+        alBuilder.setTitle("프로그램 종료");
+        alBuilder.show(); // AlertDialog.Bulider로 만든 AlertDialog를 보여준다.
     }
 }
