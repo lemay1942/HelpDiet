6af0790889e3c190a671fa148d9b5e3c72bfcb29
Min-gyu Kim <lemay1942@gmail.com>
2019-09-10 13:20:37 +0900

    main
    111

---
+++ app/src/main/java/com/example/HelpDiet/MachineActivity.java
@@ -0,0 +1,14 @@
+package com.example.HelpDiet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MachineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_machine);
+    }
+}
--- app/src/main/java/com/example/HelpDiet/MainActivity.java
+++ app/src/main/java/com/example/HelpDiet/MainActivity.java
@@ -8,26 +8,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    class BtnOnClickListner implements Button.OnClickListener {
+        @Override
+        public void onClick(View view){
+            Intent intent;
+            switch (view.getId()) {
+                case R.id.chartButton:
+                    intent = new Intent(getApplicationContext(), ChartActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.calendarButton:
+                    intent = new Intent(getApplicationContext(), CalendarActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.foodButton:
+                    intent = new Intent(getApplicationContext(), FoodActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.machineButton:
+                    intent = new Intent(getApplicationContext(), MachineActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button chartButton = (Button) findViewById(R.id.chartButton);
+        BtnOnClickListner onClickListner = new BtnOnClickListner();
 
-        chartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
+        Button chartBtn = (Button) findViewById(R.id.chartButton);
+        Button machineBtn = (Button) findViewById(R.id.machineButton);
+        Button calendarBtn = (Button) findViewById(R.id.calendarButton);
+        Button foodBtn = (Button) findViewById(R.id.foodButton);
 
-                ComponentName name = new ComponentName("com.example.HelpDiet", "com.example.HelpDiet.ChartActivity");
-
-                intent.setComponent(name);
-                startActivity(intent);
-            }
-        });
+        foodBtn.setOnClickListener(onClickListner);
+        machineBtn.setOnClickListener(onClickListner);
+        calendarBtn.setOnClickListener(onClickListner);
+        chartBtn.setOnClickListener(onClickListner);
     }
 }
